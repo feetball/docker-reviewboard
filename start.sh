c2b7f3f743dac3f52476f83d591f1f29faab866d
Daniel Kuecker <daniel@kuecker.net>
2016-05-24 07:48:02 -0500

    Update start.sh
    Converting from postgres to mysql

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-PGUSER="${PGUSER:-reviewboard}"
-PGPASSWORD="${PGPASSWORD:-reviewboard}"
-PGDB="${PGDB:-reviewboard}"
+MYSQL_USER="${MYSQL_USER}"
+MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD:-reviewboard}"
+MYSQL_PASSWORD="${MYSQL_PASSWORD:-reviewboard}"
+MYSQL_DATABASE="${MYSQL_DATABASE:-reviewboard}"
 
-# Get these variables either from PGPORT and PGHOST, or from
-# linked "pg" container.
-PGPORT="${PGPORT:-$( echo "${PG_PORT_5432_TCP_PORT:-5432}" )}"
-PGHOST="${PGHOST:-$( echo "${PG_PORT_5432_TCP_ADDR:-127.0.0.1}" )}"
+# Get these variables either from MYSQLPORT and MYSQLHOST, or from
+# linked "mysql" container.
+MYSQL_PORT="${MYSQL_PORT:-$( echo "${MYSQL_PORT_3306_TCP_PORT:-3306}" )}"
+MYSQL_HOST="${MYSQL_HOST:-$( echo "${MYSQL_PORT_3306_TCP_ADDR:-127.0.0.1}" )}"
 
 # Get these variable either from MEMCACHED env var, or from
 # linked "memcached" container.
@@ -21,19 +22,19 @@ mkdir -p /var/www/
 
 CONFFILE=/var/www/reviewboard/conf/settings_local.py
 
-if [[ ! -d /var/www/reviewboard ]]; then
+if [[ ! -d "/var/www/${DOMAIN}" ]]; then
     rb-site install --noinput \
         --domain-name="$DOMAIN" \
         --site-root=/ --static-url=static/ --media-url=media/ \
-        --db-type=postgresql \
-        --db-name="$PGDB" \
-        --db-host="$PGHOST" \
-        --db-user="$PGUSER" \
-        --db-pass="$PGPASSWORD" \
+        --db-type=mysql \
+        --db-name="${MYSQL_DATABASE}" \
+        --db-host="${MYSQL_HOST}" \
+        --db-user="${MYSQL_USER}" \
+        --db-pass="${MYSQL_PASSWORD}" \
         --cache-type=memcached --cache-info="$MEMCACHED" \
         --web-server-type=lighttpd --web-server-port=8000 \
-        --admin-user=admin --admin-password=admin --admin-email=admin@example.com \
-        /var/www/reviewboard/
+        --admin-user=admin --admin-password="${ADMIN_PASSWORD}" --admin-email=admin@example.com \
+        "/var/www/${DOMAIN}"
 fi
 if [[ "$DEBUG" ]]; then
     sed -i 's/DEBUG *= *False/DEBUG=True/' "$CONFFILE"
